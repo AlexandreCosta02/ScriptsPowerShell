67c0d86f80c85868c3f7cb40b4c76cc9e11d0e8d
Alexandre Costa <80131663+AlexandreCosta02@users.noreply.github.com>
2023-06-14 19:30:46 -0300

    Add files via upload

---
+++ WinUpdate.ps1
@@ -0,0 +1,8 @@
+﻿Install-Module PSWindowsUpdate
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+Import-Module PSWindowsUpdate
+
+Add-WUServiceManager -MicrosoftUpdate
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
+exit
+ 
