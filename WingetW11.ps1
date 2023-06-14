a0eb99d820842f60623ea65f92016b808f66df24
Alexandre Costa <80131663+AlexandreCosta02@users.noreply.github.com>
2023-06-14 20:12:02 -0300

    Add files via upload

---
+++ WingetW11.ps1
@@ -0,0 +1,29 @@
+﻿winget install -h --Accept-Package-Agreements --Accept-Source-Agreements Foxit.FoxitReader
+winget install -h --Accept-Package-Agreements --Accept-Source-Agreements Google.Chrome
+winget install -h --Accept-Package-Agreements --Accept-Source-Agreements Mozilla.Firefox
+winget install -h --Accept-Package-Agreements --Accept-Source-Agreements 7zip.7zip
+winget install -h --Accept-Package-Agreements --Accept-Source-Agreements Flameshot.Flameshot
+winget install -h --Accept-Package-Agreements --Accept-Source-Agreements Azul.Zulu.19.JDK
+winget install -h --Accept-Package-Agreements --Accept-Source-Agreements Teamviewer.Teamviewer.Host
+winget install -h --Accept-Package-Agreements --Accept-Source-Agreements Nilesoft.Shell
+winget install -h Microsoft.DotNet.DesktopRuntime.7
+winget install -h Microsoft.DotNet.DesktopRuntime.6
+winget install -h Microsoft.DotNet.DesktopRuntime.5
+winget install -h Microsoft.DotNet.Runtime.7
+winget install -h Microsoft.DotNet.Runtime.6
+winget install -h Microsoft.DotNet.Runtime.5
+winget install -h Microsoft.VCRedist.2005.x64
+winget install -h Microsoft.VCRedist.2005.x86
+winget install -h Microsoft.VCRedist.2008.x64
+winget install -h Microsoft.VCRedist.2008.x86
+winget install -h Microsoft.VCRedist.2010.x64
+winget install -h Microsoft.VCRedist.2010.x86
+winget install -h Microsoft.VCRedist.2012.x64
+winget install -h Microsoft.VCRedist.2012.x86
+winget install -h Microsoft.VCRedist.2015+.x64
+winget install -h Microsoft.VCRedist.2015+.x86
+winget install -h Microsoft.VCRedist.2013.x64
+winget install -h Microsoft.VCRedist.2013.x86
+winget install -h Microsoft.DirectX
+winget upgrade -h --all
+exit
