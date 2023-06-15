06201d972468802a06a22d2f936d0b86a9644019
Alexandre Costa <80131663+AlexandreCosta02@users.noreply.github.com>
2023-06-15 10:44:12 -0300

    Update MSR.ps1

--- MSR.ps1
+++ MSR.ps1
@@ -1,2 +1,2 @@
-﻿Get-AppXPackage -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 Get-appxpackage -all *shellexperience* -packagetype bundle |% {add-appxpackage -register -disabled developmentmode ($_.installlocation + "C:\Program Files\WindowsApps\Microsoft.Windows.ShellExperienceHost_1.0.0.2_neutral_ShellExperienceHost.laac0539cc_8wekyb3d8bbwe\AppxManifest.xml")
