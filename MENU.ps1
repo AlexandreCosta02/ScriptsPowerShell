99f795a7f7206a8f8f603004e1516a3cdde0fd85
Alexandre Costa <80131663+AlexandreCosta02@users.noreply.github.com>
2023-06-15 20:56:41 -0300

    Add files via upload

---
+++ MENU.ps1
@@ -0,0 +1,134 @@
+function DisplayMenu {
+Clear-Host
+Write-Host @"
++===========================================+
+|             MENU DE OPÇÕES                | 
++===========================================+
+|                                           |
+|    1) CPU STRESS                          |
+|    2) CTT                                 |
+|    3) MICROSOFT STORE REPAIR              |
+|    4) PADRONIZACAO W10 LIBRE NVIDIA       |
+|    5) PADRONIZACAO W10 LIBRE              |
+|    6) PADRONIZACAO W10 NVIDIA             |
+|    7) PADRONIZACAO W10                    |
+|    8) PADRONIZACAO W11 LIBRE NVIDIA       |
+|    9) PADRONIZACAO W11 LIBRE              |
+|    10) PADRONIZACAO W11 NVIDIA            |
+|    11) PADRONIZACAO W11                   |
+|    12) SLACK                              |
+|    13) WINDOWS UPDATE                     |
+|    14) SAIR                               |
+|			                    |
++===========================================+
+
+"@
+
+$MENU = Read-Host "Digite o número da opção desejada"
+Switch ($MENU)
+{
+1 {
+#OPTION1 - CPUSTRESS
+$CPUSTRESS = Invoke-WebRequest -useb https://tinyurl.com/wqe89 | Invoke-Expression
+Write-Host "$CPUSTRESS"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+2 {
+#OPTION2 - CTT
+$CTT = Invoke-WebRequest -useb https://christitus.com/win | Invoke-Expression
+Write-Host "$CTT"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+3 {
+#OPTION3 - MSR
+$MSR = Invoke-WebRequest -useb https://tinyurl.com/saiou | Invoke-Expression
+Write-Host "$MSR"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+4 {
+#OPTION4 - PWDLN
+$PWDLN = Invoke-WebRequest -useb https://tinyurl.com/saioi | Invoke-Expression
+Write-Host "$PWDLN"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+5 {
+#OPTION5 - PWDL
+$PWDL = Invoke-WebRequest -useb https://tinyurl.com/saioo | Invoke-Expression
+Write-Host "$PWDL"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+6 {
+#OPTION6 - PWDN
+$PWDN = Invoke-WebRequest -useb https://tinyurl.com/saioq | Invoke-Expression
+Write-Host "$PWDN"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+7 {
+#OPTION7 - PWD
+$PWD = Invoke-WebRequest -useb https://tinyurl.com/saiow | Invoke-Expression
+Write-Host "$PWD"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+8 {
+#OPTION8 - PWOLN
+$PWOLN = Invoke-WebRequest -useb https://tinyurl.com/saioe | Invoke-Expression
+Write-Host "$PWOLN"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+9 {
+#OPTION9 - PWOL
+$PWOL = Invoke-WebRequest -useb https://tinyurl.com/saior | Invoke-Expression
+Write-Host "$PWOL"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+10 {
+#OPTION10 - PWON
+$PWON = Invoke-WebRequest -useb https://tinyurl.com/saiod | Invoke-Expression
+Write-Host "$PWON"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+11 {
+#OPTION11 - PWO
+$PWO = Invoke-WebRequest -useb https://tinyurl.com/wgw11 | Invoke-Expression
+Write-Host "$PWO"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+12 {
+#OPTION12 - SLACK
+$SLACK = Invoke-WebRequest -useb https://tinyurl.com/saiof | Invoke-Expression
+Write-Host "$SLACK"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+13 {
+#OPTION13 - WUPDT
+$WUPDT = Invoke-WebRequest -useb https://tinyurl.com/WU653 | Invoke-Expression
+Write-Host "$WUPDT"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+14 {
+#OPTION14 - EXIT
+Write-Host "Valeu Falou"
+Break
+}
+default {
+#DEFAULT OPTION
+Write-Host "A opção digitada não existe!"
+Start-Sleep -Seconds 2
+DisplayMenu
+}
+}
+}
+DisplayMenu
